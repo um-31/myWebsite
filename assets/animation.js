5fb21696ab9b1be431ea1a8112baf4c6794c1d08
Udhay <udaymahajan97@gmail.com>
2021-06-01 13:53:37 -0400

    animation added to home screen

---
+++ assets/animation.js
@@ -0,0 +1,25 @@
+const text = ["websites","mobile apps","pancakes"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = 'n';
+
+
+(function type(){
+
+if(count === text.length ) {
+    count=0;
+}
+currentText = text[count];
+letter = currentText.slice(0, ++index);
+var element = document.querySelector('.typing');
+if (element) {
+    element.textContent = letter;
+}
+if(letter.length === currentText.length) {
+    count++;
+    index=0;
+}
+console.log(letter)
+setTimeout(type, 500);
+}());
